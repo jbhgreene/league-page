3dfa30ab7bc8183adcb4f79ecdc85d7926bc1ac6
jbhgreene <97633045+jbhgreene@users.noreply.github.com>
2022-01-12 17:15:55 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "785220608354058240"; // your league ID
 export const leagueName = "FLEX:FantasyLeague of EXathletes"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 40; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
