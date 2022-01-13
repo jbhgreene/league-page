3b142f4c6abacb270aa4058c62cac7cd8050687e
jbhgreene <97633045+jbhgreene@users.noreply.github.com>
2022-01-13 11:25:25 -0600

    Update index.svelte

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -1,4 +1,69 @@
- <p>Please refer to the <a href="https://docs.google.com/document/d/1Xhsqib1ShEx-VLC3DfK3c7JlycKQTz8uxFiBs8HiiTM/edit?usp=sharing">League Constitution</a>.</p>
+<style>
+    .constitution {
+        position: relative;
+        z-index: 1;
+        width: 92%;
+        max-width: 800px;
+        margin: 8em auto 10em;
+    }
+    h1 {
+        font-size: 2em;
+        line-height: 1.2em;
+        text-align: center;
+        margin: 2em 0 1.5em;
+    }
+    h2 {
+        font-size: 1.5em;
+        line-height: 1.2em;
+    }
+    h3 {
+        text-decoration: underline;
+        font-size: 1.3em;
+        line-height: 1.2em;
+    }
+    h4 {
+        text-decoration: underline;
+        margin-left: 2em;
+        font-size: 1.2em;
+        line-height: 1.2em;
+    }
+    h5 {
+        margin-left: 6em;
+        font-size: 0.8em;
+        line-height: 1.1em;
+    }
+    .subBlock {
+        margin-left: 2.4em;
+    }
+    .sectionHeading {
+        margin: 4em 0 1.5em;
+    }
+    .subSectionHeading {
+        margin: 1.5em 0 1.5em;
+    }
+    .underscore {
+        text-decoration: underline;
+    }
+    .right {
+        text-align: right;
+    }
+    .positionMaximums td {
+        min-width: 3em;
+    }
+    .noUnderscore {
+        text-decoration: none;
+    }
+    .clickable {
+        cursor: pointer;
+    }
+    .clickable:hover {
+        color: #00316b;
+    }
+    p {
+        color: #777;
+    }
+</style> 
+<p>Please refer to the <a href="https://docs.google.com/document/d/1Xhsqib1ShEx-VLC3DfK3c7JlycKQTz8uxFiBs8HiiTM/edit?usp=sharing">League Constitution</a>.</p>
     
 <!-- <script context="module">
 	export const prerender = true;
